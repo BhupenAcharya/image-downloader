f9edc008aa6504ab1651b4153c32287db19362f5
Bhupendra Acharya <bhupen@Bhupendras-MacBook-Air.local>
2024-09-22 09:16:41 +0100

    category

--- src/app.js
+++ src/app.js
@@ -1,10 +1,39 @@
+const { createApi } = require('unsplash-js');
+
+const unsplash = createApi({
+    accessKey: 'lpQ-f1-K-B2ailZkaWeMSKMG8lzR78wr7GVMx2ACuYo',
+});
+
 exports.handler = async (event) => {
     const url = "https://www.iana.org/help/example-domains";
     return await processUrl(url);
 };
 
+//urls.map(url => photo.urls.full.split('?')[0]);
+
 async function processUrl(url) {
-    console.log("Url is", url);
+    unsplash.search.getPhotos({
+        query: 'education, class',
+        page: 1,
+        perPage: 10,
+        orientation: 'landscape',
+    })
+        .then(result => {
+            const results = result.response.results;
+            const urls = results.map(photo => photo.urls.full.split('?')[0]);
+            console.log(urls);
+        })
+        .catch(error => {
+            console.error(error);
+        });
+
+    unsplash.search.getPhotos({
+        query: 'cat',
+        page: 1,
+        perPage: 10,
+        color: 'green',
+        orientation: 'portrait',
+    });
 }
 
 //Below code block is for testing purpose only supplying mock data:
@@ -14,7 +43,7 @@ if (require.main === module) {
 }
 
 async function main() {
-    const url='https://www.iana.org/help/example-domains';
+    const url = 'https://www.iana.org/help/example-domains';
     return await processUrl(url);
 }
 
